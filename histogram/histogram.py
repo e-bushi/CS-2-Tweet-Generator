e15b3e0dee103cfdd20c00a9f10ac28afa54bf2a
e-bushi <chrislovemauldin@gmail.com>
2017-11-06 20:16:42 -0800

    Added virtual environment directory

--- histogram/histogram.py
+++ histogram/histogram.py
@@ -1,30 +1,24 @@
 import sys
 import random
+import string
+import time
 
-def histogram(source_text):
+def read_file(source_text):
+    # returns a string
     list_of_words = []
     with open(source_text, 'r') as phile:
         data = list(phile.readlines())
 
     return data
 
-def unique_words(data):
-    ascii_numbers = [int(n) for n in range(32, 65)]
-    punctuation_and_numbers = [chr(n) for n in ascii_numbers]
-
-    more_ascii_numbers = [int(n) for n in range(91, 97)]
-    more_puncts = [chr(n) for n in more_ascii_numbers]
-
-    last_set_of_ascii = [int(n) for n in range(123, 127)]
-    last_puncts = [chr(n) for n in last_set_of_ascii]
-
-    frequency = 0
+def just_words(data):
+    # takes in a string and returns a list of unique words
     word = ""
     word_array = []
     for line_array in data:
 
         for character in line_array:
-            if character not in punctuation_and_numbers and character is not "\n" and character is not "\t" and character not in more_puncts and character not in last_puncts:
+            if character in string.ascii_letters:
                 word += character.lower()
             else:
                 if word == "":
@@ -36,7 +30,7 @@ def unique_words(data):
 
     return(word_array)
 
-def dictionary(array):
+def histogram(array):
     histogram = {}
     array_duplicate = array
 
@@ -46,20 +40,27 @@ def dictionary(array):
     return(histogram)
 
 
-def list_of_lists(array):
+def list_of_lists(words):
     "To Do: Add a familiarity array that logs every word once to prevent repetition"
     listogram = []
-    array_duplicate = array
 
-    for word in array_duplicate:
-        word_count = array.count(word)
-
-        if [word_count, word] not in listogram:
-            listogram.append([word_count, word])
+    for word in words:
+        if [1, word] not in listogram:
+            listogram.append([1, word])
         else:
-            continue
+            listogram[get_index_of_item(word, listogram)][0] += 1
 
     return sorted(listogram)
+
+def get_index_of_item(item, list_of_lists):
+    # iterate through the listogram
+    i = 0
+    for list_pair in list_of_lists:
+        # if we reach the item, return its index
+        if list_pair[1] == item:
+            return i
+        else:
+            i += 1
 
 def list_of_tuples(array):
     tuplegram = []
@@ -146,13 +147,29 @@ def generate_sentence(number_of_words, list_):
 
 if __name__ == '__main__':
     fileToRead = sys.argv[1:]
-    data = histogram(fileToRead[0])
-    array = unique_words(data)
+
+    start = time.time()
+    data = read_file(fileToRead[0])
+    print(time.time() - start)
+
+    start = time.time()
+    array = just_words(data)
+    print(time.time() - start)
     # histogram = dictionary(array)
+    start = time.time()
     listogram = list_of_lists(array)
+    print(time.time() - start)
     # tuplegram = list_of_tuples(array)
     # print(listogram)
+    start = time.time()
     total_tokens = token_total(listogram)
+    print(time.time() - start)
+
+    start = time.time()
     probability_dictionary = probability_token(total_tokens, listogram)
+    print(time.time() - start)
+
+    start = time.time()
     sentence = generate_sentence(10, probability_dictionary)
+    print(time.time() - start)
     print(sentence)
